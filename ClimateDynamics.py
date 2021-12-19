fd8fc1537ae4034ef680f75fa426813f81fe3cc6
Alessandro Sosso <alesosso@gmail.com>
2021-12-19 19:05:19 +0100

    Initial commit

---
+++ ClimateDynamics.py
@@ -0,0 +1,10 @@
+from modules.climatedynamics import ClimateDynamics
+
+
+def main():
+    cd = ClimateDynamics()
+    cd.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ modules/climatedynamics.py
@@ -0,0 +1,9 @@
+from . import var
+import climt as clm
+
+class ClimateDynamics(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
